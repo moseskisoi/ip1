d24147936f1a8e2bde164eb86b908c626620fcaf
moseskisoi <moseskisoi035@gmail.com>
2024-12-06 23:20:28 +0300

    commit 1

--- _config.js
+++ _config.js
@@ -1,9 +1,9 @@
-var config = {}
+mongodb+srv://moseskisoi:Devops@moringa@ip1.7eflb.mongodb.net/?retryWrites=true&w=majority&appName=ip1var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://moseskisoi:Devops@moringa@ip1.7eflb.mongodb.net/?retryWrites=true&w=majority&appName=ip1',
+    development: 'mongodb+srv://moseskisoi:Devops@moringa@ip1.7eflb.mongodb.net/?retryWrites=true&w=majority&appName=ip1',
+    test: 'mongodb+srv://moseskisoi:Devops@moringa@ip1.7eflb.mongodb.net/?retryWrites=true&w=majority&appName=ip1 ',
 }
 module.exports = config;
