10f72a88e2cbf899acd3e528b99df1c51bcd3c2c
moseskisoi <moseskisoi035@gmail.com>
2024-12-17 13:48:23 +0300

    Jenkins

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://moseskisoi:<db_password>@ip1.7eflb.mongodb.net/?retryWrites=true&w=majority&appName=ip1',
+    development: 'mongodb+srv://moseskisoi:<db_password>@ip1.7eflb.mongodb.net/?retryWrites=true&w=majority&appName=ip1',
+    test: 'mongodb+srv://moseskisoi:<db_password>@ip1.7eflb.mongodb.net/?retryWrites=true&w=majority&appName=ip1',
 }
 module.exports = config;
